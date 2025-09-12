dac53d23a181ca74e531149e3fb476e34c1a064e
hennncho <khetagkambolov@gmail.com>
2025-09-13 00:05:59 +0300

    Test

---
+++ include/Inventory.h
@@ -0,0 +1,8 @@
+//
+// Created by kambo on 13.09.2025.
+//
+
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#endif //INVENTORY_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Inventory.h"
 
 int main() {
   return 0;
